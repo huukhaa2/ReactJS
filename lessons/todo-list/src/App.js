998ab782bd1922190dcc14cadb265c69db02ee06
buivantan01 <buivantan1234566@gmail.com>
2021-09-02 10:25:37 +0700

    add lesson

--- lessons/todo-list/src/App.js
+++ lessons/todo-list/src/App.js
@@ -4,30 +4,52 @@ import './App.css';
 import TodoItem from './components/TodoItem';
 
 class App extends Component { // components chỉ render() khi mà props thay đổi hoặc là stage thay đổi 
-  
   constructor() {
-    
     super();
-    this.todoItems = [     // Render a list
-      { title:'đi về nhà 1', isComplete: true },
-      { title:'đi về nhà 2', isComplete: true },
-      { title:'đi về nhà 3'}
-    ];
+    this.state = {
+      todoItems: [     // Render a list
+        { title:'đi về nhà 1', isComplete: true },
+        { title:'đi về nhà 2', isComplete: true },
+        { title:'đi về nhà 3'}
+      ]
   }
+}
   
-  onItemClicked() {
-
+  onItemClicked(item) {
+    return (event) => {
+      const isComplete = item.isComplete;
+      const { todoItems } = this.state;
+      const index = todoItems.indexOf(item);
+      this.setState({
+        todoItems: [
+          ...todoItems.slice(0, index),
+          {
+            ...item,
+            isComplete: !isComplete
+          },
+          ...todoItems.slice(index + 1)
+        ]
+      });
+    }
   }
 
 
-  render() {  
-    return <div className="App">
-        {this.todoItems.length > 0 && this.todoItems.map((item, index) => (
-            <TodoItem key={index} item={item} onClick={this} />
-        ))};
-        {this.todoItems.length === 0 && 'Nothing here'}
-      </div>;
-  }
+  render(){
+    const { todoItems } = this.state;
+    if(todoItems.length) {
+      return (
+        <div className="App">
+          { todoItems.length && todoItems.map((item, index) => 
+              < TodoItem 
+                key={index} 
+                item={item} 
+                onClick={this.onItemClicked(item)} />
+            )
+          }
+        </div>
+      );
+      }else return(<div className='App'>Nothing here</div>);
+    }
 }
 
 export default App;
